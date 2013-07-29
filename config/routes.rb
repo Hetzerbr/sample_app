a9b11a8e98eecc2816bd257cd2d9f460a2fe2e3d
Hetzerbr <hetzerbr@uwec.edu>
2013-07-29 13:41:14 -0500

    fixed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-  match '/signup',  :to => 'users#new',            :via => 'get'
-  match '/static_pages/home', :to => 'static_pages#home'
-  match '/help' => 'static_pages#help',    :via => 'get' 
-  match '/about' => 'static_pages#about',   :via => 'get' 
-  match '/contact' => 'static_pages#contact', :via => 'get' 
+  root :to => 'static_pages#home'
+  match '/contact', :to => 'static_pages#contact'
+  match '/about',   :to => 'static_pages#about'
+  match '/help',    :to => 'static_pages#help'
 
-  get "static_pages/home"
 
-  get "static_pages/help"
-  
-  get "static_pages/about"
 
-  get "static_pages/contact"
+    match '/signup',  :to => 'users#new'
+
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
