5a22f3b366f15ca8b8c5c626da91e333136741f1
Hetzer <hetzerbr@lts2666736.local>
2013-07-31 14:21:53 -0500

    Fixing broken rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,30 +19,29 @@ describe User, :type => :request do
 
   describe "when name is not present" do
     before { @user.name = " " }
-    it { should_not be_valid } 
+    it { should_not be_valid }
+  end
   
 
-    describe "when email is not present" do
+  describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
-  
+  end
 
-    describe "when name is too long" do
+  describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
-    
+  end
  
- describe "when email address is already taken" do
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
+    end
 
     it { should_not be_valid }
-      end
-    end
   end
-end
 
  describe "when email format is invalid" do
     it "should be invalid" do
@@ -65,7 +64,7 @@ end
     end
   end
 
-describe "email address with mixed case" do
+  describe "email address with mixed case" do
     let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
     it "should be saved as all lower-case" do
@@ -75,44 +74,39 @@ describe "email address with mixed case" do
     end
   end
 
-describe "when password is not present" do
+  describe "when password is not present" do
     before do
       @user = User.new(:name => "Example User", :email => "user@example.com",
                        :password => " ", :password_confirmation => " ")
-    
+    end
 
     it { should_not be_valid }
   end
-end
 
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     
-
     it { should_not be_valid }
   end
-end
 
-describe "with a password that's too short" do
+  describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
 
   describe "return value of authenticate method" do
     before { @user.save }
-    let(:found_user) { User.find_by(:email => @user.email) }
-  end
+    let(:found_user) { User.find_by_email @user.email }
 
     describe "with valid password" do
       it { should eq found_user.authenticate(@user.password) }
     end
   
-
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
+  
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
-      end
     end
-  
+  end
+end  
